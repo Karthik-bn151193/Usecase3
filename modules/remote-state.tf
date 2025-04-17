3a2449df378aa5f7c8239fc80c1444ff4ec46ea9
Karthik-bn151193 <karthik.bn15111993@gmail.com>
2025-04-17 17:24:31 +0530

    Update remote-state.tf

--- modules/remote-state.tf
+++ modules/remote-state.tf
@@ -1,1 +1,10 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "usecasehcl170425"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+    use_lockfile   = true
+    encrypt        = true
+  }
+}
